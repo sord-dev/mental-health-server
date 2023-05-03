3b2cc8b282e7f0757a87334bd17f1936ac1b7c34
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-05-03 13:03:14 +0100

    adding user model

---
+++ models/User.js
@@ -0,0 +1,11 @@
+class User {
+    constructor({id, username, password}){
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    static create(data) {
+        db.query()
+    }
+}
