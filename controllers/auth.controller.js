b88a6e072a2a86fe668fd9e097c4d921308ac55f
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-05-03 19:53:20 +0100

    adding better user model error handling and tweaking auth controllers

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,25 +1,26 @@
 const User = require('../models/User');
 
 module.exports.register = async (req, res) => {
-    if (!req.body.username || !req.body.password) return res.status(422).json({ error: 'Incorrect Input' });
+    if (!req.body.username || !req.body.password) return res.status(422).json({ message: 'Incorrect Input' });
 
     try {
         const user = await User.create(req.body);
 
         res.status(201).json({ ...user, password: null });
     } catch (error) {
-        res.status(422).json({ error: error.message })
+        if(error.code == 23505) return res.status(422).json({ message: 'User already exists' })
+        res.status(422).json({ message: error.message })
     }
 }
 
 module.exports.login = async (req, res) => {
-    if (!req.body.username || !req.body.password) return res.status(422).json({ error: 'Incorrect Input' });
+    if (!req.body.username || !req.body.password) return res.status(422).json({ message: 'Incorrect Input' });
     try {
         const user = await User.findByUsername(req.body.username)
 
         if (User.comparePasword(req.body.password, user.password)) res.status(200).json({ ...user, password: null })
         else res.status(401).json({ error: 'Incorrect Password' });
     } catch (error) {
-        res.status(500).json({ error: error.message })
+        res.status(400).json({ message: error.message })
     }
 } 
