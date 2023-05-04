4cfee553818e763bfa76629c53bcd3eaeb3bbfc6
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-05-04 18:01:25 +0100

    adding mentor chat short term memory

--- routes/mentor.route.js
+++ routes/mentor.route.js
@@ -1,25 +1,23 @@
 const router = require('express').Router();
 const ChatGPT = require('../models/ChatGPT')
-const mem = [];
+let mem = []; // chat short term memory
 
-router.get('/', async (req, res) => {
-    try {
-        const response = await ChatGPT.generateMentorChat("Hey Mike")
-
-        res.json(response)
-    } catch (error) {
-        res.json({ error: error.message })
-    }
-})
-
-router.post('/chat', async (req, res) => {
+router.post('/chat', async (req, res) => { // {message: {content: input, role: 'user'}}
     let { message } = req.body;
     mem.push(message)
 
-    let messages = mem.map(m => m.content).join('\n');
+    let messages = mem.map(m => (`${m.role == 'assistant' ? 'Morgan: ' : 'User: '} ${m.content}`)).join('\n') + '\n Morgan:'; // parse all previous messages into a string
+    let parsedHistory = `Continue this conversation: \n ${messages}`; // ask chatgpt to continue this conversation given all the previous messages
+    
+    if(mem.length > 40) mem = [] // hard limit to chat memory
+    console.log(parsedHistory);
+    // when messages get too long
+    // summarise conversation and save messages to db
+
 
     try {
-        const response = await ChatGPT.generateMentorChat(messages);
+        const response = await ChatGPT.generateMentorChat(parsedHistory);
+        mem.push({ role: 'assistant', content: response })
 
         res.status(200).json({ message: response });
     } catch (error) {
