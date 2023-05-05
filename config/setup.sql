ff7b6f2627db481365c09bf3f2c9c90e4e8800bf
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-05-05 10:59:42 +0100

    not dabloons

--- config/setup.sql
+++ config/setup.sql
@@ -10,6 +10,8 @@ CREATE TABLE users (
   password VARCHAR(140) NOT NULL,
   created_at timestamp DEFAULT CURRENT_TIMESTAMP,
   dabloons INT DEFAULT 0,
+  goals JSON,
+  mentor VARCHAR(120) DEFAULT '53f57cad12a2c697226b96c10f12db1d',
   PRIMARY KEY (user_id)
 );
 
@@ -20,7 +22,7 @@ CREATE TABLE shop (
     price INT,
     user_id INT,
     PRIMARY KEY (shop_id),
-    FOREIGN KEY (user_id) REFERENCES users("user_id")
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE forums (
@@ -33,12 +35,12 @@ CREATE TABLE forums (
     CURRENT_TIMESTAMP,
     user_id INT,
     PRIMARY KEY (forum_id),
-    FOREIGN KEY (user_id) REFERENCES users("user_id")
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE comments (
     comment_id INT GENERATED ALWAYS AS IDENTITY,
-    content VARCHAR(250),
+    comment VARCHAR(250),
     created_at timestamp DEFAULT 
     CURRENT_TIMESTAMP,
     update_at timestamp DEFAULT 
@@ -46,8 +48,8 @@ CREATE TABLE comments (
     user_id INT,
     forum_id INT,
     PRIMARY KEY (comment_id),
-    FOREIGN KEY (user_id) REFERENCES users("user_id"),
-    FOREIGN KEY (forum_id) REFERENCES forums("forum_id") ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (forum_id) REFERENCES forums(forum_id) ON DELETE CASCADE
 );
 
 CREATE TABLE goals (
@@ -60,5 +62,5 @@ CREATE TABLE goals (
     CURRENT_TIMESTAMP,
     user_id INT,
     PRIMARY KEY (goal_id),
-    FOREIGN KEY (user_id) REFERENCES users("user_id")
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
--- routes/game.route.js
+++ routes/game.route.js
@@ -1,5 +1,48 @@
 const router = require('express').Router();
 const ChatGPT = require('../models/ChatGPT')
+const DallE = require('../models/DallE')
 
+router.get('/initiateGame', async (req, res) => {
+    try {
+        const initialPrompt = await ChatGPT.generateGamePromptResponse("I have mixed feelings about myself. Help me to understand them.")
+        const dallE = await DallE.generateImage(initialPrompt.dall_e_ink)
+        res.json(dallE)
+    } catch (error) {
+        res.json({ error: error.message })
+    }
+})
+
+router.post('/createImage', async (req, res) => {
+    try {
+        const givenPrompt = req.body.prompt;
+
+        const prompt = await DallE.generateImage(givenPrompt)
+        res.json(prompt)
+    }
+    catch (error) {
+        res.json({ error: error.message })
+    }
+})
+
+router.post('/checkAnswer', async (req, res) => {
+    try {
+        console.log(req.body)
+        const prompt = await ChatGPT.generateGamePromptResponse(req.body.answer)
+        res.json(prompt)
+    }
+    catch (error) {  
+        res.json({ error: error.message })
+    }
+})
+
+router.get('/', async (req, res) => {
+    try {
+        const response = await ChatGPT.generateMentorChat("I very much like my life at the moment it's quite nice!")
+
+        res.json(response)
+    } catch (error) {
+        res.json({ error: error.message })
+    }
+})
 
 module.exports = router;
