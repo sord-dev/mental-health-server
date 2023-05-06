85c1d30313eaa50b9735a7d7d55d4238b717a8e9
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-05-06 21:06:03 +0100

    pain

--- controllers/forumController.js
+++ controllers/forumController.js
@@ -1,94 +1,90 @@
 const Forum = require("../models/Forum")
 
 async function index(req, res) {
-    try {
-        const forums = await Forum.getAll();
-        res.status(200).json(forums);
+  try {
+    const forums = await Forum.getAll();
+    res.status(200).json(forums);
 
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 }
 
 async function getById(req, res) {
-    try {
-        const id = parseInt(req.params.id);
-        const forum = await Forum.getOneById(id);
-        res.status(200).json(forum);
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
+  try {
+    const id = parseInt(req.params.id);
+    const forum = await Forum.getOneById(id);
+    res.status(200).json(forum);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 }
 
 async function getByUserId(req, res) {
-    try {
-      const id = parseInt(req.params.id);
-      const forums = await Forum.getAllCreatedByUser(id);
-  
-      if (forums.length === 0) {
-        throw new Error(`User with ID ${id} not found`);
-      }
-  
-      res.status(200).json(forums);
-    } catch (err) {
-      res.status(404).json({ error: err.message });
+  try {
+    const id = parseInt(req.params.id);
+    const forums = await Forum.getAllCreatedByUser(id);
+
+    if (forums.length === 0) {
+      throw new Error(`User with ID ${id} not found`);
     }
+
+    res.status(200).json(forums);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
   }
-  
+}
+
 
 async function create(req, res) {
-    const data = req.body;
-    
-    if (!data.title || !data.content) {
-        res.status(400).json({ error: 'Failed to create forum: title and content are required' });
-    } else {
-        try {
-            
-            const forum = await Forum.create(data);
-            res.status(201).json(forum);
-        } catch (err) {
-            const errorMessage = `Failed to create forum: ${err.message}`;
-            res.status(500).json({ error: errorMessage });
-        }
-    }
+  const data = req.body;
+  if(!data.title) return res.status(422).json({error: 'Incorrect Input, Please Provide Title.'})
+  try {
+    const forum = await Forum.create(data);
+    res.status(201).json(forum);
+  } catch (err) {
+    const errorMessage = `Failed to create forum: ${err.message}`;
+    res.status(500).json({ error: errorMessage });
+  }
+
 }
 
 async function destroy(req, res) {
-    try {
-        const id = parseInt(req.params.id);
-        const forum = await Forum.getOneById(id);
+  try {
+    const id = parseInt(req.params.id);
+    const forum = await Forum.getOneById(id);
 
-       
-        
 
-        const result = await forum.destroy();
-        res.status(204).json({result, message: 'Forum post deleted successfully' });
-    } catch (err) {
-        res.status(404).json({ error: err.message })
-    }
+
+
+    const result = await forum.destroy();
+    res.status(204).json({ result, message: 'Forum post deleted successfully' });
+  } catch (err) {
+    res.status(404).json({ error: err.message })
+  }
 }
 
 
 async function update(req, res) {
-    try {
-      const id = parseInt(req.params.id);
-      const { title, content } = req.body;
-      
-      if (!title && !content) {
-        return res.status(400).json({ error: 'Failed to update forum post: title or content are required' });
-      }
-      
-      const forum = await Forum.updateOneById(id, { title, content });
-  
-      if (!forum) {
-        return res.status(404).json({ error: 'Forum post not found' });
-      }
-  
-      res.status(200).json({ forum: forum, message: 'Forum post updated successfully' });
-    } catch (err) {
-      res.status(500).json({ error: err.message });
+  try {
+    const id = parseInt(req.params.id);
+    const { title, content } = req.body;
+
+    if (!title && !content) {
+      return res.status(400).json({ error: 'Failed to update forum post: title or content are required' });
     }
+
+    const forum = await Forum.updateOneById(id, { title, content });
+
+    if (!forum) {
+      return res.status(404).json({ error: 'Forum post not found' });
+    }
+
+    res.status(200).json({ forum: forum, message: 'Forum post updated successfully' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
-  
+}
+
 
 module.exports = { index, destroy, create, getByUserId, getById, update };
--- utils/gpttools/commands/aimentors/index.js
+++ utils/gpttools/commands/aimentors/index.js
@@ -2,13 +2,13 @@ const fs = require("fs");
 
 const AiPersonalities = {
     "Morgan": { prompt: fs.readFileSync(__dirname + "/default.txt").toString(), price: 0, category: 'Default' },
-    "David": { prompt: fs.readFileSync(__dirname + "/david.txt").toString(), price: 120000, category: 'Wise' },
-    "Snoop": { prompt: fs.readFileSync(__dirname + "/snoop.txt").toString(), price: 200000, category: 'Musician' },
-    "Bob": { prompt: fs.readFileSync(__dirname + "/bob.txt").toString(), price: 200000, category: 'Nice' },
-    "Stephen": { prompt: fs.readFileSync(__dirname + "/stephen.txt").toString(), price: 200000, category: 'Science' },
-    "Lex": { prompt: fs.readFileSync(__dirname + "/lex.txt").toString(), price: 200000, category: 'CompSci' },
-    "Shakespeare": { prompt: fs.readFileSync(__dirname + "/shakespeare.txt").toString(), price: 200000, category: 'Wise' },
-    "Swift": { prompt: fs.readFileSync(__dirname + "/swift.txt").toString(), price: 200000, category: 'Musician' }
+    "David Attenborough": { prompt: fs.readFileSync(__dirname + "/david.txt").toString(), price: 120000, category: 'Wise' },
+    "Snoop Dogg": { prompt: fs.readFileSync(__dirname + "/snoop.txt").toString(), price: 200000, category: 'Musician' },
+    "Bob Ross": { prompt: fs.readFileSync(__dirname + "/bob.txt").toString(), price: 200000, category: 'Nice' },
+    "Stephen Hawking": { prompt: fs.readFileSync(__dirname + "/stephen.txt").toString(), price: 200000, category: 'Science' },
+    "Lex Friedman": { prompt: fs.readFileSync(__dirname + "/lex.txt").toString(), price: 200000, category: 'CompSci' },
+    "William Shakespeare": { prompt: fs.readFileSync(__dirname + "/shakespeare.txt").toString(), price: 200000, category: 'Wise' },
+    "Taylor Swift": { prompt: fs.readFileSync(__dirname + "/swift.txt").toString(), price: 200000, category: 'Musician' }
 }
 
 
