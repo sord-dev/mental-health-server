b3c25899ff464028a5fadcc5372a2953e3aefe03
MisterCxmpy <cosgunhussein@gmail.com>
2023-05-06 15:50:16 +0100

    create memeory for each persona and change objects

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 const controller = require('../controllers/user.contoller.js')
 
 // POST /user/pts - update points
-router.post('/pts', controller.updatePoints);
-router.post('/goals', controller.updateGoals);
+router.patch('/pts', controller.updatePoints);
+router.patch('/goals', controller.updateGoals);
+router.patch('/mentor', controller.updateMentor);
 router.get("/:id", controller.getById)
 
 module.exports = router;
