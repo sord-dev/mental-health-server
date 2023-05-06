c0bd4b52e24bca08e6198bf555143e76e5e79712
MisterCxmpy <cosgunhussein@gmail.com>
2023-05-06 14:10:18 +0100

    prompt stuff

--- routes/mentor.route.js
+++ routes/mentor.route.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 const ChatGPT = require('../models/ChatGPT')
 
+const { Mentors } = require('../utils/gpttools/commands/aimentors')
+
 let mem = []; // chat short term memory
 router.post('/chat', async (req, res) => { // {message: {content: input, role: 'user'}, mentor: 378278931278hijdfa }
     let { message, mentor } = req.body;
     mem.push(message)
 
-    let messages = mem.map(m => (`${m.role == 'assistant' ? 'Morgan: ' : 'User: '} ${m.content}`)).join('\n') + '\n Morgan:'; // parse all previous messages into a string
+    let messages = mem.map(m => (`${m.role == 'assistant' ? `${Mentors[mentor]}: ` : 'User: '} ${m.content}`)).join('\n') + `\n ${Mentors[mentor]}:`; // parse all previous messages into a string
     let parsedHistory = `Continue this conversation: \n ${messages}`; // ask chatgpt to continue this conversation given all the previous messages
     
     if(mem.length > 40) mem = [] // hard limit to chat memory
